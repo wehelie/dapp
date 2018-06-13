ac2f3e8173c66f640c1ccbdfdc6e589be6ddcb8c
wehelie <mawehelie@gmail.com>
2018-06-13 01:24:13 -0700

    comment unused methods

--- app/src/main/java/com/example/wehelie/dapp/ProfileFragment.java
+++ app/src/main/java/com/example/wehelie/dapp/ProfileFragment.java
@@ -1,20 +1,15 @@
 package com.example.wehelie.dapp;
 
-import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
-import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
-import android.view.View.OnClickListener;
-
-
-import android.net.Uri;
 
 public class ProfileFragment extends Fragment {
 
@@ -71,26 +66,26 @@ public class ProfileFragment extends Fragment {
         }
     }
 
-    public static class ContentAdapter extends RecyclerView.Adapter<ViewHolder> {
-        private static final int LENGTH = 18;
-
-        public ContentAdapter() {
-        }
-
-        @Override
-        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            return new ViewHolder(LayoutInflater.from(parent.getContext()), parent);
-        }
-
-        @Override
-        public void onBindViewHolder(ViewHolder holder, int position) {
-        }
-
-        @Override
-        public int getItemCount() {
-            return LENGTH;
-        }
-    }
+//    public static class ContentAdapter extends RecyclerView.Adapter<ViewHolder> {
+//        private static final int LENGTH = 18;
+////
+////        public ContentAdapter() {
+////        }
+//
+//        @Override
+//        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+//            return new ViewHolder(LayoutInflater.from(parent.getContext()), parent);
+//        }
+//
+//        @Override
+//        public void onBindViewHolder(ViewHolder holder, int position) {
+//        }
+//
+//        @Override
+//        public int getItemCount() {
+//            return LENGTH;
+//        }
+//    }
 
 
 
